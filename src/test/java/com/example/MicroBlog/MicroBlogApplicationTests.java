36388e82ff9b28803670f4ed78c42409e9d7c823
Edoardo <edo.allari@gmail.com>
2020-03-14 18:57:37 +0100

    Initialized repository

---
+++ src/test/java/com/example/MicroBlog/MicroBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MicroBlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
