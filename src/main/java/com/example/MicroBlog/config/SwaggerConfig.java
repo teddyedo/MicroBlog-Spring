e719c2fcb30a8a3b8aa0ed45e5117255b98e0aef
Edoardo <edo.allari@gmail.com>
2020-03-19 22:19:44 +0100

    created swaggeConfig

---
+++ src/main/java/com/example/MicroBlog/config/SwaggerConfig.java
@@ -0,0 +1,29 @@
+package com.example.microblog.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import static springfox.documentation.builders.PathSelectors.regex;
+
+/**
+ * SwaggerConfig
+ */
+
+ @Configuration
+ @EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean 
+    public Docket productApi(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.example.microblog"))
+                .paths(regex("/api.*"))
+                .build();
+    }
+}
