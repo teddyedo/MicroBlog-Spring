c7e71f523e9356804d0537c448c30482ba5d37bc
Edoardo <edo.allari@gmail.com>
2020-03-20 09:56:23 +0100

    Added a first version of swagger docs

--- src/main/java/com/example/MicroBlog/config/SwaggerConfig.java
+++ src/main/java/com/example/MicroBlog/config/SwaggerConfig.java
@@ -3,12 +3,15 @@ package com.example.microblog.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-import static springfox.documentation.builders.PathSelectors.regex;
 
 /**
  * SwaggerConfig
@@ -18,12 +21,23 @@ import static springfox.documentation.builders.PathSelectors.regex;
  @EnableSwagger2
 public class SwaggerConfig {
 
-    @Bean 
-    public Docket productApi(){
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("com.example.microblog"))
-                .paths(regex("/api.*"))
-                .build();
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2).select()
+            .apis(RequestHandlerSelectors
+                .basePackage("com.example.microblog.restApi"))
+            .paths(PathSelectors.regex("/.*"))
+            .build().apiInfo(apiEndPointsInfo());
+    }
+
+    private ApiInfo apiEndPointsInfo() {
+        
+        return new ApiInfoBuilder().title("Spring Boot REST API")
+            .description("Employee Management REST API")
+            .contact(new Contact("Allari Edoardo", "www.exampleSite.net", "contactme@gmail.com"))
+            .license("Apache 2.0")
+            .licenseUrl("http://www.apache.org/licenses/LICENSE-2.0.html")
+            .version("1.0.0")
+            .build();
     }
 }
