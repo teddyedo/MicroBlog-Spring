27eedf749a98f63dae77257fa386e7e3cb58bc40
Edoardo <edo.allari@gmail.com>
2020-03-21 17:40:47 +0100

    implemented mysql database

--- src/main/java/com/example/MicroBlog/security/SecurityConfiguration.java
+++ src/main/java/com/example/MicroBlog/security/SecurityConfiguration.java
@@ -1,9 +1,14 @@
-package com.example.microblog.security;
+/*package com.example.microblog.security;
 
-/*import org.springframework.context.annotation.Bean;
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -14,23 +19,44 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
+
+
+    @Autowired
+    DataSource dataSource;
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception{
 
-        auth.inMemoryAuthentication()
-            .withUser("gino")
-            .password("gino")
-            .roles("USER")
-            .and()
-            .withUser("gervaso")
-            .password("gervaso")
-            .roles("ADMIN");
-            
+        auth.jdbcAuthentication()
+            .dataSource(dataSource)
+            .withDefaultSchema()
+            .withUser(
+                User.withUsername("user")
+                .password("pass")
+                .roles("USER")
+            )
+            .withUser(
+                User.withUsername("admin")
+                .password("admin")
+                .roles("ADMIN")
+            );
     }
-    
+
+
+
 
     @Bean
     public PasswordEncoder getPasswordEncoder(){
         return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception{
+        
+        http.authorizeRequests()
+            .antMatchers("/Microblog/posts/n*").hasRole("ADMIN")
+            .antMatchers("Microblog/comments/n*").hasAnyRole("USER", "ADMIN")
+            .antMatchers("/", "resources/static/css", "resources/static/fonts", "resources/static/js", "resources/static/vendor").permitAll()
+            .and().formLogin();
     }
 }*/
