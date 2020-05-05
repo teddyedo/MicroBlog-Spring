2767483bc2bacb0be147192d97ba2ee1730bba1f
Edoardo <edo.allari@gmail.com>
2020-05-05 20:32:46 +0200

    refactor

--- src/main/java/com/example/microblog/MicroBlogApplication.java
+++ src/main/java/com/example/microblog/MicroBlogApplication.java
@@ -11,10 +11,8 @@ import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-//import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-//@EnableSwagger2
 @EnableJpaRepositories
 public class MicroBlogApplication {
 
