2fb85f0475e4edfaf1ebafbdd8f72812da22609e
Edoardo <edo.allari@gmail.com>
2020-05-05 22:27:36 +0200

    refactor

--- src/main/java/com/example/microblog/springDataRest/RepoUser.java
+++ src/main/java/com/example/microblog/springDataRest/RepoUser.java
@@ -1,8 +1,10 @@
 package com.example.microblog.springDataRest;
 
 import com.example.microblog.entities.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.webmvc.support.RepositoryEntityLinks;
 
 import java.util.Optional;
 
@@ -10,4 +12,5 @@ import java.util.Optional;
 public interface RepoUser extends CrudRepository<User, Long> {
 
     Optional<User> findByUsername(String username);
+
 }
